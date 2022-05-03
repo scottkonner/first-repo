2a19093f94d3b44e01ed9b17ddd8ac0bf1c4245f
Scott <scott_koner@yahoo.com>
2022-05-03 19:33:19 -0400

    first commit fourever

---
+++ problems/03-tripler.js
@@ -0,0 +1,20 @@
+/*******************************************************************************
+Write a function tripler(array) that takes in an array and returns a new array
+containing 3 times every element of the original array.
+
+Examples:
+
+tripler([1,2,3]); // => [ 3, 6, 9 ]
+tripler([4, 1, 7]); // => [ 12, 3, 21 ]
+*******************************************************************************/
+
+function tripler(array) {
+  let tripled = []
+  for (let i = 0; i<array.length; i++) {
+    let big = (array[i] * 3)
+    tripled.push(array[i] )
+  }
+  return tripled
+}
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = tripler;
